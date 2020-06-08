9d9ea5e1d2932f06aed638b5b158914c0d0cd232
Eirik-Nicolai <eirik.n.o.oseberg@gmail.com>
2020-06-08 21:00:01 +0200

    Added two player boxes controlled via websocket. Thank you Tech with Tim

--- client.py
+++ client.py
@@ -1,2 +1,85 @@
 import pygame
 import network_helper
+
+game_width  = 500
+game_height = 500
+
+print("starting up...")
+
+window = pygame.display.set_mode((game_width, game_height))
+pygame.display.set_caption("Client")
+
+print("pygame initiated")
+
+class Player():
+    def __init__(self, posx, posy, width, height, colour):
+        self.x = posx
+        self.y = posy
+        self.width = width
+        self.height = height
+        self.colour = colour
+
+        self.rect = (self.x, self.y, self.width, self.height)
+        self.vel = 1
+
+    def draw(self, window):
+        pygame.draw.rect(window, self.colour, self.rect)
+
+    def move(self):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_LEFT]:
+            self.x -= self.vel
+
+        if keys[pygame.K_RIGHT]:
+            self.x += self.vel
+
+        if keys[pygame.K_UP]:
+            self.y += self.vel
+
+        if keys[pygame.K_DOWN]:
+            self.y -= self.vel
+
+        self.update()
+
+    def update(self):
+        self.rect = (self.x, self.y, self.width, self.height)
+
+
+def frame_tick(window, p1, p2):
+    window.fill((255,255,255))
+    p1.draw(window)
+    p2.draw(window)
+    pygame.display.update()
+
+def read_pos(str):
+    str = str.split(",")
+    return int(str[0]),int(str[1])
+
+def make_pos(pos):
+    return str(pos[0]) + "," + str(pos[1])
+
+def main():
+    run = True
+    n = network_helper.Helper()
+    start_pos = read_pos(n.get_pos())
+    p1 = Player(start_pos[0],start_pos[1], 40, 40, (255,0,0))
+    p2 = Player(0, 0, 40, 40, (0, 100, 255))
+    clock = pygame.time.Clock()
+
+    while run:
+        clock.tick(60)
+        p2_pos = read_pos(n.send(make_pos((p1.x,p1.y))))
+        p2.x = p2_pos[0]
+        p2.y = p2_pos[1]
+        p2.update()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                pygame.quit()
+                break;
+        p1.move()
+        frame_tick(window, p1, p2)
+
+main()
--- server.py
+++ server.py
@@ -1,3 +1,60 @@
 import socket
 import sys
 from _thread import *
+
+server = "192.168.0.4"
+port = 5555
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+try:
+    s.bind((server, port))
+except socket.error as e:
+    str(e)
+
+print("set up server")
+
+s.listen(2)
+print("Listening...")
+
+def read_pos(str):
+    val = str.split(',')
+    return int(val[0]),int(val[1])
+
+def make_pos(pos):
+    return str(pos[0]) + "," + str(pos[1])
+
+pos = [(0,0), (150,150)]
+
+def threaded_client(conn, player):
+    conn.send(str.encode(make_pos(pos[player])))
+    reply = ""
+    while True:
+        try:
+            data = read_pos(conn.recv(2048).decode())
+            pos[player] = data
+
+            if not data:
+                break
+            else:
+                if player == 0:
+                    reply = pos[1]
+                else:
+                    reply = pos[0]
+
+
+                conn.sendall(str.encode(make_pos(reply)))
+
+        except socket.error as e:
+            print("error 3 ", e)
+            break
+    print("Terminating connection")
+    conn.close()
+
+current_player = 0
+while True:
+    conn, addr = s.accept()
+    print("Connected to ", addr)
+
+    start_new_thread(threaded_client, (conn, current_player))
+    current_player += 1
